a9bbdc93965c72f316224c49a8ae5b5e680a4986
dev-parthive <dev-parthive>
2022-11-29 14:47:40 +0600

    basic  router setup done

--- src/routes/Routes/Routes.js
+++ src/routes/Routes/Routes.js
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom"
 import Error from "../../pages/ErrorPage/Error"
 import Main from '../../layout/Main'
 import Home from "../../pages/Home/Home";
+import Blog from "../../pages/BLog/Blog";
+import Login from "../../pages/Login/Login";
 const router = createBrowserRouter([
     {
         path: '/', 
@@ -15,7 +17,16 @@ const router = createBrowserRouter([
             {
                 path: '*', 
                 element: <Error></Error>
-            }
+            }, 
+           {
+            path: '/blog', 
+            element: <Blog></Blog>
+           } , 
+           {
+            path: '/login', 
+            element: <Login></Login>
+
+           }
         ]
     }
 ])
