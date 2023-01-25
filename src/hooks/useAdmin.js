652cc79583d80a19795f427771868da8e7ce9b31
dev-parthive <dev-parthive>
2023-01-25 12:46:33 +0600

    done

--- src/hooks/useAdmin.js
+++ src/hooks/useAdmin.js
@@ -4,17 +4,17 @@ const useAdmin = email =>{
     const [isAdmin , setIsAdmin] = useState(false)
     const [isAdminLoading, setIsAdminLoading] = useState(true)
 
-    // useEffect( ()=>{
-    //     if(email){
-    //         fetch(`https://y-dev-parthive.vercel.app/users/admin/${email}`)
-    //         .then(res => res.json())
-    //         .then(data =>{
-    //             console.log(data)
-    //             setIsAdmin(data.isdAdmin)
-    //             setIsAdminLoading(false)
-    //         })
-    //     }
-    // }, [email]);
+    useEffect( ()=>{
+        if(email){
+            fetch(`https://y-dev-parthive.vercel.app/users/admin/${email}`)
+            .then(res => res.json())
+            .then(data =>{
+                console.log(data)
+                setIsAdmin(data.isdAdmin)
+                setIsAdminLoading(false)
+            })
+        }
+    }, [email]);
 
 
 
